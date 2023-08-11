c137fdbb4c85ac809f49553ffe64fb738d2ff5d6
Angel Victor Juanco Muller <angelvictorjuancomuller@Angels-MacBook-Air.local>
2023-08-11 22:20:16 +0100

    improved it

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const burger = document.querySelector("nav svg")
 
 burger.addEventListener("click", () => {
+    burger.classList.toggle("active")
+
+//    if(burger.classList.contains("active")){
     // alert("hello");
-   if(burger.classList.contains("active")){
-    gsap.to(".links", {x:"100%"});
-   }else{
-    gsap.to(".links",{x: "0%"});
-   }
-   burger.classList.toggle("active")
+    // gsap.to(".links", {x:"100%"});
+//     links.x = "100%"
+//    }else{
+//     links.x = "0%"
+//    }
+if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "0%" });
+    // alert("hello");
+  } else {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
 });
